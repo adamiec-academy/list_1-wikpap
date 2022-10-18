9012052e6e78df8dc9d2795e83c6e0b403fcf48c
wikpap <115736945+wikpap@users.noreply.github.com>
2022-10-18 19:19:46 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,14 @@
 def cross(n):
     pass
+w = 4
+for i in range(w):
+    print(4 * " " + w * "*" + 4 * " ")
+
+def print_rectangle(a , b):
+    for i in range(b):
+        print(a * "*")
+print_rectangle(12,4)
+
+w = 4
+for i in range(w):
+    print(4 * " " + w * "*" + 4 * " ")
