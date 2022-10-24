45fb9845ad84b9aba149aa63152fc99436d03dc3
wikpap <115736945+wikpap@users.noreply.github.com>
2022-10-24 13:02:33 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,8 @@
 def chess_board(n, k):
-    pass
+    for i in range(n):
+        for _ in range(k):
+            print((k * " " + k * "#") * n)
+        for _ in range(k):
+            print((k * "#" + k * " ") * n)
+
+print(chess_board(4, 2))
