2251195869bbe263d350222aea50f509ce522ddb
wikpap <115736945+wikpap@users.noreply.github.com>
2022-11-21 14:42:26 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,12 @@
 def factorial(n):
-    pass
-
+    acc = 1
+    for i in range(1, n + 1):
+        acc *= i
+    return acc
 
 def report():
-    pass
+    acc = 1
+    print(f"{0 : >3}! is {1 : >3} digits long")
+    for i in range(1, 101):
+        acc *= i
+        print(f"{i : >3}! is {len(str(acc)) : >3} digits long")
