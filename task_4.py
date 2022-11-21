ef60442b25a94cb930f1eb58f8fb1fc964c11c8e
wikpap <115736945+wikpap@users.noreply.github.com>
2022-11-21 14:25:04 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,19 @@
 def snowball(n, k):
-    pass
+ offset = 0.5
+    ray = n/2
 
+    for x in range(n):
+        print(k * " ", end="")
+        for y in range(n):
+            if (x + offset - ray) ** 2 + (y + offset - ray) ** 2 <= ray ** 2:
+                print("#", end="")
+            else:
+                print(" ", end="")
+        print()
 
 def snowman(n):
-    pass
+     snowball(n, 2)
+    snowball(n + 2, 1)
+    snowball(n + 4, 0)
+
+snowman(7)
